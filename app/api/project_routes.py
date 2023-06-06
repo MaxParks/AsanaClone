894dda1e98cae47a0f7b20c088798c551187d797
Sydney Cendana <103895652+sydneycendana@users.noreply.github.com>
2023-06-05 21:37:35 -0700

    giving team owner access to edit project

--- app/api/project_routes.py
+++ app/api/project_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
+from sqlalchemy.orm import joinedload
 from flask_login import login_required, current_user
-from app.models import db, Project, User, Task, UserTeam
+from app.models import db, Project, User, Task, Team, UserTeam
 from app.forms import ProjectForm
 from datetime import datetime
 
@@ -16,22 +17,11 @@ def validation_errors_to_error_messages(validation_errors):
             errorMessages.append(f'{field} : {error}')
     return errorMessages
 
-@project_routes.route('/<int:id>', methods=['GET'])
-@login_required
-def retrieve_project(id):
 
-    project = Project.query.get(id)
-
-    if not project:
-        return {"message": "Project not found", "statusCode": 404}, 404
-
+def get_team_members_dict(project):
     team_id = project.team_id
 
     user_ids = [user_id[0] for user_id in UserTeam.query.filter_by(team_id=team_id).distinct().values(UserTeam.user_id)]
-
-    if current_user.id != project.owner_id:
-        if current_user.id not in user_ids:
-                    return {"message": "Unauthorized", "statusCode": 403}, 403
 
     team_members = User.query.filter(User.id.in_(user_ids)).all()
 
@@ -44,24 +34,45 @@ def retrieve_project(id):
         }
         team_members_dict[member.id] = user_info
 
+    return team_members_dict
+
+
+# -------------- GET PROJECT --------------------
+@project_routes.route('/<int:id>', methods=['GET'])
+@login_required
+def retrieve_project(id):
+
+    project = Project.query.get(id)
+
+    if not project:
+        return {"message": "Project not found", "statusCode": 404}, 404
+
+    team_members_dict = get_team_members_dict(project)
+
+    if current_user.id != project.owner_id:
+        if current_user.id not in team_members_dict:
+                    return {"message": "Unauthorized", "statusCode": 403}, 403
+
+    team_id = project.team_id
+    team = Team.query.filter_by(id=team_id).options(joinedload('owner')).first()
+    owner_id = team.owner_id
+    print('-----------------', owner_id)
+
     project_dict = project.to_dict()
+    project_dict['tasks'] = []
 
-    if project:
-        project_dict = project.to_dict()
-        project_dict['tasks'] = []
+    sorted_tasks = sorted(project.tasks, key=lambda task: task.due_date, reverse=False)
 
-        sorted_tasks = sorted(project.tasks, key=lambda task: task.due_date, reverse=False)
-
-        for task in sorted_tasks:
-            task_dict = {
-                'id': task.id,
-                'name': task.name,
-                'description': task.description,
-                'due_date': task.due_date.isoformat(),
-                'completed': task.completed,
-                'assigned_to': task.assigned_to,
-            }
-            project_dict['tasks'].append(task_dict)
+    for task in sorted_tasks:
+        task_dict = {
+            'id': task.id,
+            'name': task.name,
+            'description': task.description,
+            'due_date': task.due_date.isoformat(),
+            'completed': task.completed,
+            'assigned_to': task.assigned_to,
+        }
+        project_dict['tasks'].append(task_dict)
 
         project_dict['team_members'] = team_members_dict
 
@@ -71,7 +82,7 @@ def retrieve_project(id):
 
 
 
-
+# -------------- CREATE PROJECT --------------------
 @project_routes.route('/', methods=['POST'])
 @login_required
 def create_project():
@@ -100,19 +111,21 @@ def create_project():
 
 
 
+# -------------- UPDATE PROJECT --------------------
 @project_routes.route('/<int:id>', methods=['PUT'])
 @login_required
 def update_project(id):
-    """
-    Updates a specific project by its ID.
-    """
     project = Project.query.get(id)
     if not project:
         return {"message": "Project not found", "statusCode": 404}, 404
 
-    # Check if the current user is the owner of the project
     if current_user.id != project.owner_id:
         return {"message": "Unauthorized", "statusCode": 403}, 403
+
+    team_id = project.team_id
+    team = Team.query.filter_by(id=team_id)
+    owner_id = team.owner_id
+    print('-----------------', owner_id)
 
     data = request.get_json()
     name = data.get('name')
