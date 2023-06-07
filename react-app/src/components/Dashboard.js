9df062ed4374f2d5669007c08c76ac527f8aeab4
MaxParks <novarecon@yahoo.com>
2023-06-07 06:18:39 -0700

    working on dashboard

--- react-app/src/components/Dashboard.js
+++ react-app/src/components/Dashboard.js
@@ -7,6 +7,38 @@ import "./Navigation/Navigation.css";
 function Dashboard({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
 
+  // Fake data for tasks and projects
+  const tasks = [
+    { id: 1, name: "Task 1" },
+    { id: 2, name: "Task 2" },
+    { id: 3, name: "Task 3" },
+  ];
+
+  const projects = [
+    { id: 1, name: "Project 1" },
+    { id: 2, name: "Project 2" },
+    { id: 3, name: "Project 3" },
+  ];
+
+  const currentDate = new Date();
+  const dateOptions = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  };
+  const formattedDate = currentDate.toLocaleDateString(undefined, dateOptions);
+
+  const getGreetingMessage = () => {
+    const currentHour = currentDate.getHours();
+    if (currentHour < 12) {
+      return "Good morning";
+    } else if (currentHour < 18) {
+      return "Good afternoon";
+    } else {
+      return "Good evening";
+    }
+  };
+
   return (
     <div
       className="page-container"
@@ -15,6 +47,101 @@ function Dashboard({ isLoaded }) {
       <div className="header-container">
         <h1 className="header-title">Home</h1>
         <ProfileButton user={sessionUser} />
+      </div>
+
+      <div
+        className="dashboard-container"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          color: "white",
+        }}
+      >
+        <div
+          className="dashboard-header"
+          style={{
+            width: "100%",
+            textAlign: "center",
+            marginBottom: "1rem",
+          }}
+        >
+          <h2 className="dashboard-date" style={{ fontSize: "20px" }}>
+            {formattedDate}
+          </h2>
+          <h3 className="dashboard-greeting" style={{ fontSize: "1.5rem" }}>
+            {`${getGreetingMessage()}, ${sessionUser.firstName}`}
+          </h3>
+        </div>
+
+        <div
+          className="dashboard-content"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <div
+            className="tasks-container"
+            style={{
+              backgroundColor: "grey",
+              boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.1)",
+              padding: "20px",
+              marginRight: "20px",
+              width: "80%",
+              borderRadius: "8px",
+            }}
+          >
+            <h2 className="section-title" style={{ marginTop: "0" }}>
+              My Tasks
+            </h2>
+            <div className="task-list" style={{ marginTop: "10px" }}>
+              {tasks.map((task) => (
+                <div
+                  className="task-item"
+                  key={task.id}
+                  style={{
+                    padding: "5px",
+                    marginBottom: "5px",
+                    color: 'white',
+                    borderRadius: "4px",
+                  }}
+                >
+                  {task.name}
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className="projects-container"
+            style={{
+              backgroundColor: "grey",
+              boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.1)",
+              padding: "20px",
+              marginLeft: "20px",
+              width: "80%",
+              borderRadius: "8px",
+            }}
+          >
+            <h2 className="section-title" style={{ marginTop: "0" }}>
+              Projects
+            </h2>
+            <div className="project-list" style={{ marginTop: "10px" }}>
+              {projects.map((project) => (
+                <div
+                  className="project-item"
+                  key={project.id}
+                  style={{
+                    padding: "5px",
+                    marginBottom: "5px",
+                    color: 'white',
+                    borderRadius: "4px",
+                  }}
+                >
+                  {project.name}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
