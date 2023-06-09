c90e011c7eb5df5f4a1783e3ae51c7a6fc98a6eb
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-06-08 20:06:51 -0400

    got teams loading + their data

---
+++ react-app/src/components/Teams/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTeams} from "../../store/teams"
+import { getDashboardThunk } from '../../store/dashboard'
+import { useParams } from "react-router-dom";
+// import "./Team.css";
+
+
+const Team = () => {
+  const dispatch = useDispatch()
+  const { id } = useParams()
+
+  const dashboardData = useSelector(state => state.dashboard)
+  const teamData = dashboardData.teams ? dashboardData.teams[id] : null
+  console.log('TEAM DATA --->', teamData)
+
+  useEffect(() => {
+    dispatch(getDashboardThunk())
+  }, [dispatch])
+
+  if (!teamData) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <h1>Team: {teamData.name}</h1>
+      <h2>Members:</h2>
+      <ul>
+        {teamData.members.map(memberId => (
+          <li key={memberId}>Member ID: {memberId}</li>
+        ))}
+      </ul>
+      <h2>Projects:</h2>
+      <ul>
+        {teamData.projects.map(projectId => (
+          <li key={projectId}>Project ID: {projectId}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Team
