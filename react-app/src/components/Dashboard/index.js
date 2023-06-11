bc5e680ca0f57cd6c92a67d6361be47c440b950d
Sydney Cendana <103895652+sydneycendana@users.noreply.github.com>
2023-06-11 14:50:05 -0700

    projects dashboard css

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -12,6 +12,9 @@ import AddTaskModal from "../Tasks/AddTaskModal";
 import TaskModal from "../Tasks/TaskModal";
 
 import { ReactComponent as Checkmark } from "../../assets/icons/checkmark.svg";
+import { ReactComponent as ProjectIcon } from "../../assets/icons/project-icon.svg";
+import { ReactComponent as Plus } from "../../assets/icons/plus.svg";
+
 import "./Dashboard.css";
 
 function Dashboard() {
@@ -118,25 +121,33 @@ function Dashboard() {
         <div className="split-container">
           <div className="section-title-container">
             <h2 className="section-title">Projects:</h2>
-            <div className="project-list">
-              {dashboardData.projects &&
-                Object.values(dashboardData.projects).map((project) => (
-                  <Link
-                    to={`/projects/${project.id}`}
-                    key={project.id}
-                    className="project-item"
-                  >
-                    {project.name}
-                  </Link>
-                ))}
-                <div className="create-project-button">
+          </div>
+          <div className="project-list">
+            <div className="create-project-button">
               <OpenModalButton
-                buttonText="Add New Project"
+                buttonText="+"
                 modalComponent={<CreateProjectModal />}
-                className="button-CreateProjectModal"
+                className="create-project"
+              />
+              <OpenModalButton
+                buttonText="Create a Project"
+                modalComponent={<CreateProjectModal />}
+                className="create-project-text"
               />
             </div>
-            </div>
+            {dashboardData.projects &&
+              Object.values(dashboardData.projects).map((project) => (
+                <Link
+                  to={`/projects/${project.id}`}
+                  key={project.id}
+                  className="project-item"
+                >
+                  <div className="project-icon-wrapper">
+                    <ProjectIcon className="project-svg-icon" />
+                  </div>
+                  {project.name}
+                </Link>
+              ))}
           </div>
         </div>
       </div>
